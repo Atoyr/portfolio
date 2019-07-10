e11bef0688f2eb159a717154fb339b1292f055e3
atoyr <lessironglance@gmail.com>
2019-07-10 23:54:40 +0900

    add ui framework element-ui

--- nuxt.config.js
+++ nuxt.config.js
@@ -22,12 +22,14 @@ export default {
   /*
   ** Global CSS
   */
-  css: [
+    css: [
+    'element-ui/lib/theme-chalk/index.css'
   ],
   /*
   ** Plugins to load before mounting the App
   */
   plugins: [
+    '@/plugins/element-ui'
   ],
   /*
   ** Nuxt.js modules
